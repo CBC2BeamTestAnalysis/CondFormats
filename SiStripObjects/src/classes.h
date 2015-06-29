aad61c7bf4a4116ae299f258d66fd4024f89c2a2
kskovpen <Kirill.Skovpen@cern.ch>
2015-06-29 08:58:59 +0200

    first commit

---
+++ SiStripObjects/src/classes.h
@@ -0,0 +1,36 @@
+#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+#include "CondFormats/SiStripObjects/interface/FedChannelConnection.h"
+#include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
+#include "CondFormats/SiStripObjects/interface/SiStripThreshold.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+#include "CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBackPlaneCorrection.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBadStrip.h"
+#include "CondFormats/SiStripObjects/interface/SiStripDetVOff.h"
+#include "CondFormats/SiStripObjects/interface/SiStripLatency.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBaseDelay.h"
+#include "CondFormats/SiStripObjects/interface/SiStripRunSummary.h"
+#include "CondFormats/SiStripObjects/interface/SiStripSummary.h"
+#include "CondFormats/SiStripObjects/interface/SiStripConfObject.h"
+#include "CondFormats/SiStripObjects/interface/Phase2TrackerModule.h"
+#include "CondFormats/SiStripObjects/interface/Phase2TrackerCabling.h"
+
+namespace {
+  struct dictionary {
+    std::vector< std::vector<FedChannelConnection> > tmp1;
+  
+#ifdef SISTRIPCABLING_USING_NEW_STRUCTURE
+  
+//    SiStripFedCabling::Registry            temp12;
+
+#endif
+   
+    std::vector<SiStripThreshold::Container>  tmp22;
+    std::vector< SiStripThreshold::DetRegistry >  tmp24;
+
+    std::vector<Phase2TrackerModule> vp2m;
+ 
+  };
+}  
+  
